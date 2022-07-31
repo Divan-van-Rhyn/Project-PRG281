71bdcb95f614d77962dd0f9e96decfbff0470a09
Divan van Rhyn <dvanrhyn@outlook.com>
2022-07-31 17:52:45 +0200

    Classes connecting database to forms

---
+++ DataService/Models/SalesData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Project_PRG281.DataService.Models
+{
+    [Table("Sales")]
+    class SalesData
+    {
+        [Key]
+        [Required]
+        public int SalesID { get; set; }
+
+        [Required]
+        public int ProductID { get; set; }
+
+        [Required]
+        public int UserID { get; set; }
+    }
+}
